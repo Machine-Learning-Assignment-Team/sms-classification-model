efb6a946bc26507331d3fd5fe4ed1479b1ef81b1
‪Sagi <57183982+sagagaga4@users.noreply.github.com>
2026-04-12 14:42:33 +0300

    added GridSearchCV and evaluation metrics
    implement Naive Bayes with hyperparameter tuning

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-# קובץ main.py זמני לבדיקה
 import numpy as np
 from model_logic import train_and_evaluate_model
 
-print("מייצר נתונים שיקריים לחלוטין לבדיקת הלוגיקה")
+print("Dummy training set")
 X_train_dummy = np.random.rand(100, 50) 
-y_train_dummy = np.random.randint(2, size=100) # 0 או 1
+y_train_dummy = np.random.randint(2, size=100) 
 
 X_test_dummy = np.random.rand(20, 50)
 y_test_dummy = np.random.randint(2, size=20)
--- src/model_logic.py
+++ src/model_logic.py
@@ -1,13 +1,30 @@
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.model_selection import GridSearchCV # הפונקציה שעושה אופטימיזציה ואימות צולב
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def train_and_evaluate_model(x_train, y_train, x_test, y_test):
+        
+    base_model = MultinomialNB()
+
+    param_grid = {
+        'alpha': [0.01, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0]
+    }
+
+    grid_search = GridSearchCV(
+        estimator=base_model, 
+        param_grid=param_grid, 
+        cv=5, 
+        scoring='f1'
+    )
     
-    model = MultinomialNB()
-    model.fit(x_train, y_train)
+    grid_search.fit(x_train, y_train)
     
-    y_prediction_train = model.predict(x_train)
-    y_prediction_test = model.predict(x_test)
+    best_model = grid_search.best_estimator_
+    
+    print(f"Optimized params {grid_search.best_params_}\n")
+    
+    y_prediction_train = best_model.predict(x_train)
+    y_prediction_test = best_model.predict(x_test)
     
     metrics = {
         "Train" : {
@@ -15,7 +32,7 @@ def train_and_evaluate_model(x_train, y_train, x_test, y_test):
             "Precision": precision_score(y_train, y_prediction_train, pos_label=1),
             "Recall": recall_score(y_train, y_prediction_train, pos_label=1),
             "F1-Score": f1_score(y_train, y_prediction_train, pos_label=1)
-    },
+        },
         "Test": {
             "Accuracy": accuracy_score(y_test, y_prediction_test),
             "Precision": precision_score(y_test, y_prediction_test, pos_label=1),
@@ -25,18 +42,18 @@ def train_and_evaluate_model(x_train, y_train, x_test, y_test):
     }
     
     print("-" * 40)
-    print("Train Result:")
+    print("Train Result (Using Best Naive Bayes Model):")
     print(f"Accuracy:  {metrics['Train']['Accuracy']:.4f}")
     print(f"Precision: {metrics['Train']['Precision']:.4f}")
     print(f"Recall:    {metrics['Train']['Recall']:.4f}")
     print(f"F1-Score:  {metrics['Train']['F1-Score']:.4f}")
     print("-" * 40)
     
-    print("Test Result:")
+    print("Test Result (Using Best Naive Bayes Model):")
     print(f"Accuracy:  {metrics['Test']['Accuracy']:.4f}")
     print(f"Precision: {metrics['Test']['Precision']:.4f}")
     print(f"Recall:    {metrics['Test']['Recall']:.4f}")
     print(f"F1-Score:  {metrics['Test']['F1-Score']:.4f}")
     print("-" * 40)
     
-    return model, metrics
+    return best_model, metrics
